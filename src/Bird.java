4a01b5fe55baf3d1f3b20af92fb51c4e129ab4dc
chengzhaoxu <chngzhxu@163.com>
2020-09-04 11:50:21 +0800

    dev

--- src/Bird.java
+++ src/Bird.java
@@ -1,5 +1,13 @@
 public class Bird {
     public static void main(String[] args) {
-        System.out.println("Hi, Bird");
+        float a = 8, b = 29;
+        float sum = sum(a, b);
+
+        System.out.println("Hi, Bird : " + sum);
+    }
+
+
+    private static float sum(float a, float b){
+        return a + b;
     }
 }
